6118554a5993ea6a765e022130a0120145683b6b
Alexradch24 <57526284+Alexradch24@users.noreply.github.com>
2023-04-19 17:25:40 +0000

    создание отпуска

--- GA_code.py
+++ GA_code.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def date_trans(date_string):
     year, month, day = map(int, date_string.strip().split('.'))
@@ -69,6 +70,70 @@ def hol_eval_func (w, h_date_start, h_date_end, w_date_start, w_date_end):
 def gr_eval_func():
     return 1
 
+def distr_holid(i):
+    #выбираем начало длиного отпуска
+    ind = random.randint(0,len(kalendar) - 10)
+    day_start_end_personal_holidays.append([i, kalendar[ind], kalendar[ind + 9]])
+    #выбираем начало короткого отпуска
+    if ind - 10 < 0:
+        ind1 = random.randint(ind + 15, len(kalendar) - 5)
+    elif ind + 20 > len(kalendar):
+        ind1 = random.randint(0, ind - 10)
+    else:
+        ind1_1 = random.randint(0, ind - 10)
+        ind1_2 = random.randint(ind + 15, len(kalendar) - 5)
+        ind1 = random.choice([ind1_1, ind1_2])
+    day_start_end_personal_holidays.append([i, kalendar[ind1], kalendar[ind1 + 4]])
+    buf = ind - ind1
+    if buf < 20 and buf > -25:
+        if buf > 0:
+            if ind1 - 10 < 0:
+                ind2 = random.randint(ind + 15, len(kalendar) - 5)
+            elif ind + 20 > len(kalendar):
+                ind2 = random.randint(0, ind1 - 10)
+            else:
+                ind2_1 = random.randint(0, ind1 - 10)
+                ind2_2 = random.randint(ind + 15, len(kalendar) - 5)
+                ind2 = random.choice([ind2_1, ind2_2])
+        else:
+            if ind - 10 < 0:
+                ind2 = random.randint(ind1 + 10, len(kalendar) - 5)
+            elif ind1 + 15 > len(kalendar):
+                ind2 = random.randint(0, ind - 10)
+            else:
+                ind2_1 = random.randint(0, ind - 10)
+                ind2_2 = random.randint(ind1 + 10, len(kalendar) - 5)
+                ind2 = random.choice([ind2_1, ind2_2])
+    else:
+        if buf > 0:
+            if ind1 - 10 < 0:
+                ind2_2 = random.randint(ind + 15, len(kalendar) - 5)
+                ind2_3 = random.randint(ind1 + 10, ind - 10)
+                ind2 = random.choice([ind2_2, ind2_3])
+            elif ind + 20 > len(kalendar):
+                ind2_1 = random.randint(0, ind1 - 10)
+                ind2_3 = random.randint(ind1 + 10, ind - 10)
+                ind2 = random.choice([ind2_1, ind2_3])
+            else:
+                ind2_1 = random.randint(0, ind1 - 10)
+                ind2_2 = random.randint(ind + 15, len(kalendar) - 5)
+                ind2_3 = random.randint(ind1 + 10, ind - 10)
+                ind2 = random.choice([ind2_1, ind2_2, ind2_3])
+        else:
+            if ind - 10 < 0:
+                ind2_2 = random.randint(ind1 + 10, len(kalendar) - 5)
+                ind2_3 = random.randint(ind + 15, ind1 - 10)
+                ind2 = random.choice([ind2_2, ind2_3])
+            elif ind1 + 15 > len(kalendar):
+                ind2_1 = random.randint(0, ind - 10)
+                ind2_3 = random.randint(ind + 15, ind1 - 10)
+                ind2 = random.choice([ind2_1, ind2_3])
+            else:
+                ind2_1 = random.randint(0, ind - 10)
+                ind2_2 = random.randint(ind1 + 10, len(kalendar) - 5)                
+                ind2_3 = random.randint(ind + 15, ind1 - 10)
+                ind2 = random.choice([ind2_1, ind2_2, ind2_3])
+    day_start_end_personal_holidays.append([i, kalendar[ind2], kalendar[ind2 + 4]])
 
 kalendar = [i for i in range(1,366)]
 file = open("holidays2023.txt", "r")
@@ -77,8 +142,12 @@ for i in range(len(weekends)):
     day_int = date_trans(weekends[i])
     ind = kalendar.index(day_int)
     kalendar = kalendar[:ind]+kalendar[ind + 1:]
-print(kalendar)
+#print(kalendar)
 
-a = hol_eval_func(1, 1, 8, 1, 7)
-print(a)
-
+count_personal = 6
+count_personal_holidays = [20] * count_personal
+day_start_end_personal_holidays = []
+for i in range(count_personal):
+    distr_holid(i)
+for i in day_start_end_personal_holidays:
+    print(i)
