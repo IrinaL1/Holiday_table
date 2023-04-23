c2145a7212b6afd7838659041b633199214577e9
Alexradch24 <57526284+Alexradch24@users.noreply.github.com>
2023-04-23 20:39:38 +0000

    почти конец

--- GA_code.py
+++ GA_code.py
@@ -31,52 +31,11 @@ def date_trans(date_string):
         n_date = day + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30
     return n_date
 
-k1 =1
-k2 = 1
-k3 = 1
-
-
-def hol_eval_func (w, h_date_start, h_date_end, w_date_start, w_date_end):
-    if (h_date_end - h_date_start <= 0):
-        print("Wrong holiday. Change start = ", h_date_start, "and end date = ", h_date_end, "or check the lenght\n")
-        return -1
-    if (w_date_end - w_date_start <= 0):
-        print("Wrong holiday. Change start = ", w_date_start, "and end date = ", w_date_end, "or check the lenght\n")
-        return -1
-    if (w == 0):
-        return 1
-    if (w == 0.5):
-        w_date_start = w_date_start - 5
-        w_date_end = w_date_end + 5
-        if (h_date_start - w_date_start >= 0 and w_date_end - h_date_end >= 0):
-            return 1
-        elif (h_date_end - w_date_start >= 0 and h_date_end - w_date_end <= 0):
-            return (h_date_end - w_date_start + 1)/(h_date_end - h_date_start + 1)
-        elif (h_date_start - w_date_start >= 0 and h_date_start - w_date_end <= 0):
-            return (w_date_end - h_date_start + 1)/(h_date_end - h_date_start + 1)
-        else:
-            return 0
-    elif (w == 1):
-        if (h_date_start - w_date_start >= 0 and w_date_end - h_date_end >= 0):
-            return 1
-        elif (h_date_end - w_date_start >= 0 and h_date_end - w_date_end <= 0):
-            return (h_date_end - w_date_start + 1)/(h_date_end - h_date_start + 1)
-        elif (h_date_start - w_date_start >= 0 and h_date_start - w_date_end <= 0):
-            return (w_date_end - h_date_start + 1)/(h_date_end - h_date_start + 1)
-        else:
-            return 0
-    else:
-        print("Wrong value of w. It must be 0, 0.5 or 1.\n")
-        return -1
-
-def gr_eval_func():
-    return 1
-
 def distr_holid(i):
     global day_start_end_personal_holidays
     #выбираем начало длиного отпуска
     ind = random.randint(0,len(kalendar) - 10)
-    b = lib.create(kalendar[ind], kalendar[ind + 9], i)
+    b = lib.create_H(kalendar[ind], kalendar[ind + 9], i)
     day_start_end_personal_holidays.append(b)
     #выбираем начало короткого отпуска, если его нельзя вставить перед длинным
     if ind - 10 < 0:
@@ -89,7 +48,7 @@ def distr_holid(i):
         ind1_1 = random.randint(0, ind - 10)
         ind1_2 = random.randint(ind + 15, len(kalendar) - 5)
         ind1 = random.choice([ind1_1, ind1_2])
-    b = lib.create(kalendar[ind1], kalendar[ind1 + 4], i)
+    b = lib.create_H(kalendar[ind1], kalendar[ind1 + 4], i)
     day_start_end_personal_holidays.append(b)
     buf = ind - ind1
     #если не можем вставить между отпусками
@@ -164,7 +123,7 @@ def distr_holid(i):
                 ind2_2 = random.randint(ind1 + 10, len(kalendar) - 5)                
                 ind2_3 = random.randint(ind + 15, ind1 - 10)
                 ind2 = random.choice([ind2_1, ind2_2, ind2_3])
-    b = lib.create(kalendar[ind2], kalendar[ind2 + 4], i)
+    b = lib.create_H(kalendar[ind2], kalendar[ind2 + 4], i)
     day_start_end_personal_holidays.append(b)
    # return day_start_end_personal_holidays
 
@@ -208,6 +167,7 @@ def reproduction():
     
 
 kalendar = [i for i in range(1,366)]
+work_days = [[] for i in range(12)]
 file = open("holidays2023.txt", "r")
 weekends = list(map(str, file.read().strip().split()))
 for i in range(len(weekends)):
@@ -215,10 +175,23 @@ for i in range(len(weekends)):
     ind = kalendar.index(day_int)
     kalendar = kalendar[:ind]+kalendar[ind + 1:]
 #print(kalendar)
+for i in kalendar:
+    if i <= 31: work_days[0].append(i)
+    elif i <= 31 + 28: work_days[1].append(i)
+    elif i <= 31 + 28 + 31: work_days[2].append(i)
+    elif i <= 31 + 28 + 31 + 30: work_days[3].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31: work_days[4].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30: work_days[5].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30 + 31: work_days[6].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31: work_days[7].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30: work_days[8].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31: work_days[9].append(i)
+    elif i <= 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30: work_days[10].append(i)
+    else: work_days[11].append(i)
 
 lib = ctypes.CDLL('./lib.so')
-lib.create.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
-lib.create.restype = ctypes.c_void_p
+lib.create_H.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
+lib.create_H.restype = ctypes.c_void_p
 lib.get_start_date.argtypes = [ctypes.c_void_p]
 lib.get_start_date.restype = ctypes.c_int
 lib.get_end_date.argtypes = [ctypes.c_void_p]
@@ -227,15 +200,17 @@ lib.get_person.argtypes = [ctypes.c_void_p]
 lib.get_person.restype = ctypes.c_int
 lib.get_v.argtypes = [ctypes.c_void_p]
 lib.get_v.restype = ctypes.c_double
-#lib.hol_eval.argtypes = [ctypes.c_void_p, ctypes.c_double, ctypes.c_void_p]
-#lib.hol_eval.restype = ctypes.c_double
+lib.hol_eval.argtypes = [ctypes.c_void_p, ctypes.c_double, ctypes.c_char_p]
+lib.hol_eval.restype = ctypes.c_double
+lib.set_v.argtypes = [ctypes.c_void_p, ctypes.c_double]
+lib.create_S.argtypes = [ctypes.c_void_p]
 
-count_personal = 40
+count_personal = 5
 count_personal_holidays = [20] * count_personal
 day_start_end_personal_holidays = []
 population = []
 wishes = []
-for j in range(100):
+for j in range(10):
     for i in range(count_personal):
         distr_holid(i)
     population.append(copy.deepcopy(day_start_end_personal_holidays))
@@ -251,10 +226,12 @@ k = 0
 for i in population:
     print(k)
     k += 1
+    string = "12-31;56-78; 99-119"
+    a = ctypes.create_string_buffer(str.encode(string))
     for j in i:
         print(lib.get_start_date(j), end = ' ')
         print(lib.get_end_date(j), end = ' ')
         print(lib.get_person(j), end = ' ')
-        print(lib.get_v(j))
+        print(lib.hol_eval(j, 1, a))
     print("-------------------------------------------------")
-
+    
